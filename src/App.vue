d13487afaec22730d53b40aae717ac2ffd61dd73
unknown <siddharthpadwal3@gmail.com>
2022-03-08 02:10:33 +0530

    created basic audiobok reader using vue options api

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,63 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <video
+        @playing="videoPlay"
+        ref="video"
+        width="720"
+        height="560"
+        autoplay
+        muted
+    ></video>
+    <pre ref="text"></pre>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+    methods: {
+        async videoPlay() {
+            const worker = Tesseract.createWorker();
+            await worker.load();
+            await worker.loadLanguage("eng");
+            await worker.initialize("eng");
+            const canvas = document.createElement("canvas");
+            canvas.width = this.$refs.video.width;
+            canvas.height = this.$refs.video.height;
+            document.addEventListener("keypress", async (e) => {
+                if (e.code !== "Space") return;
+                canvas
+                    .getContext("2d")
+                    .drawImage(
+                        this.$refs.video,
+                        0,
+                        0,
+                        this.$refs.video.width,
+                        this.$refs.video.height
+                    );
+                const {
+                    data: { text },
+                } = await worker.recognize(canvas);
+
+                speechSynthesis.speak(
+                    new SpeechSynthesisUtterance(text.replace(/\s/g, " "))
+                );
+
+                this.$refs.text.textContent = text;
+            });
+        },
+    },
+    async created() {
+        const stream = await navigator.mediaDevices.getUserMedia({
+            video: true,
+        });
+        this.$refs.video.srcObject = stream;
+    },
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+pre {
+    font-size: 2rem;
+    font-family: inherit;
+    width: 100%;
+    white-space: pre-wrap;
 }
 </style>
